3a8ba0e077a86a7238659c98af4f375d4d2769a4
xrdmz51-ux <xrdmz51@gmail.com>
2026-09-18 12:23:37 +0900

    3주차

--- unity-gg/Assets/playercontroller.cs
+++ unity-gg/Assets/playercontroller.cs
@@ -1,15 +1,48 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
  
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public string playerName = "Player";
+    public int hp = 100;
+        public float moveSpeed = 5f;
+    public float jumpPower = 8f;
+    public Vector3 startPosition;
+    public Transform visual; 
     private Vector2 moveInput;
+    private Rigidbody2D rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = startPosition;
+        Debug.Log(playerName + "시작.체력" +hp);
+        Debug.Log("피격 후 체력 " + (hp - 30));
+        Debug.Log("달리기 속도 " + moveSpeed * 2);
+        Debug.Log("10 / 4 = " + 10 / 4);
+    }
+    //아래 Onmove,Update는 그대로
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+        if (moveInput.x > 0)
+        {
+            visual.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            visual.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+ void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Debug.Log("점프!");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        }
     }
 
     void Update()
